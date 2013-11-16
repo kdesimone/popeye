2a520d8900f9a2d26b1a46183cc012e77e30342b
arokem <arokem@gmail.com>
2013-11-16 06:33:05 -0800

    For now, not importing config, so the package can be imported.

--- pRF_analysis/__init__.py
+++ pRF_analysis/__init__.py
@@ -5,8 +5,8 @@ __docformat__ = 'restructuredtext'
 
 from .version import  __version__
 
-from . import config
-from . import estimation
+# from . import config
+# from . import estimation
 from . import fitting
 from . import utilities
 from . import MakeFastPrediction
