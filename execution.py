9682f5071f7a333cb04b5dc6455a079ae3115d47
kdesimone <kevindesimone@gmail.com>
2013-11-19 14:14:04 -0500

    small edit to execution file

--- execution.py
+++ execution.py
@@ -1,13 +1,8 @@
 import datetime
-import time
-import ctypes
 import numpy as np
-import nibabel
+
 from config import init_config
-from utilities import multiprocessor, recast_simulation_results_queue, generate_shared_array, recast_estimation_results_queue
-from estimation import compute_prf_estimate
-from simulation import simulate_neural_sigma
-from reconstruction import multiprocess_stimulus_reconstruction
+import pRF_analysis as prf
 
 # initialize the datasets as per config.py
 stimData,funcData,metaData = init_config()
@@ -20,10 +15,10 @@ metaData['voxels'] = tuple((xi[randInd],yi[randInd],zi[randInd]))
 
 # # Run the pRF estimation
 tic = datetime.datetime.now()
-output = multiprocessor(compute_prf_estimate,stimData,funcData,metaData)
+output = prf.utilities.multiprocessor(prf.estimation.compute_prf_estimate,stimData,funcData,metaData)
 toc = datetime.datetime.now()
 print("The pRF estimation took %s" %(toc-tic))
  
 # # Write the results out as nifti_gz
-recast_estimation_results_queue(output,metaData,True)
+prf.utilities.recast_estimation_results_queue(output,metaData,True)
 
