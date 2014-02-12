90ee43be1632b30540f4fc4e0f46691a1ceb2a7b
kdesimone <kevindesimone@gmail.com>
2014-02-12 18:59:02 -0500

    ENH: added a base class for pRF models and fits.  also added a particular instance of the pRF models in pRF (gaussian).

---
+++ popeye/base.py
@@ -0,0 +1,29 @@
+
+"""
+
+Base-classes for poulation encoding models and fits.
+
+
+"""
+
+
+class PopulationModel(object):
+    """ Abstract class for signal reconstruction models
+    """
+    def __init__(self, stimulus):
+        """
+
+        """
+        self.stimulus = stimulus
+
+    def fit(self, data, mask=None,**kwargs):
+        return PopulationModel(self, data)
+
+class PopulationFit(object):
+    """ Abstract class which holds the fit result of ReconstModel
+
+    For example that could be holding FA or GFA etc.
+    """
+    def __init__(self, model, data):
+        self.model = model
+        self.data = data
