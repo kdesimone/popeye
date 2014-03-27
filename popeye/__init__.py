6eab51c07b19577c5e3fa3d5317e2220fc0e6e13
kdesimone <kevindesimone@gmail.com>
2014-03-27 01:50:08 -0400

    RF: modified the __init__.py to make it more contemporary

--- popeye/__init__.py
+++ popeye/__init__.py
@@ -6,6 +6,5 @@ __docformat__ = 'restructuredtext'
 from .version import  __version__
 
 from . import stimulus
-from . import reconstruction
-from . import simulation
 from . import utilities
+from . import prf
