5d410034704fd96cd9ef75cca936375d619888b8
kdesimone <kevindesimone@gmail.com>
2014-04-28 14:45:16 -0400

    removed the fit method from the PopulationModel base class

--- popeye/base.py
+++ popeye/base.py
@@ -7,18 +7,11 @@ Base-classes for poulation encoding models and fits.
 
 
 class PopulationModel(object):
-    """ Abstract class for signal reconstruction models
+    """ Abstract class which holds the stimulus model
     """
     
     def __init__(self, stimulus):
-        """
-
-        """
         self.stimulus = stimulus
-
-    def fit(self, data, mask=None,**kwargs):
-        
-        return PopulationFit(self, data, mask)
 
 class PopulationFit(object):
     """ Abstract class which holds the fit result of PopulationModel
