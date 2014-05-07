08b7b3f869b8da54c5676f97124711b3193fe8f3
kdesimone <kevindesimone@gmail.com>
2014-05-07 10:18:51 -0400

    TEST: modifications to the STRF fitting method and test

--- popeye/tests/test_strf.py
+++ popeye/tests/test_strf.py
@@ -9,6 +9,7 @@ from scipy.signal import chirp
 import popeye.utilities as utils
 import popeye.strf as strf
 from popeye.auditory_stimulus import AuditoryStimulus
+from popeye.spinach import MakeFastGaussian2D
 
 def test_double_gamma_hrf():
     """
@@ -41,7 +42,7 @@ def test_error_function():
     fs = hi_freq*2 # Hz
     duration = 100 # seconds
     tr_length = 1.0 # seconds
-    time_window = 0.5 # seconds
+    time_window = 0.1 # seconds
     freq_window = 256 # this is 2x the number of freq bins we'll end up with in the spectrogram
     
     num_timepoints = np.floor(duration / tr_length)
@@ -112,7 +113,7 @@ def test_ballpark_brute_force():
     stimulus = AuditoryStimulus(signal, tr_length, freq_window, time_window)
     
     # set up bounds for the grid search
-    bounds = ((lo_freq, hi_freq),(lo_freq, hi_freq),(0,tr_length),(-5,5))
+    bounds = ((lo_freq, hi_freq),(lo_freq, hi_freq),(-5,5))
     
     # initialize the gaussian model
     strf_model = strf.SpectrotemporalModel(stimulus)
@@ -120,11 +121,10 @@ def test_ballpark_brute_force():
     # makeup a STRF estimate
     freq_center = 550 # center frequency
     freq_sigma = 100 # frequency dispersion
-    time_sigma = 0.250 # # seconds
     hrf_delay = 0.0 # seconds
     
     # generate the modeled BOLD response
-    response = strf.compute_model_ts(freq_center, freq_sigma, time_sigma, hrf_delay,
+    response = strf.compute_model_ts(freq_center, freq_sigma, hrf_delay,
                                      stimulus.time_coord, stimulus.freq_coord, stimulus.spectrogram,
                                      tr_length, num_timepoints, time_window, norm_func=utils.zscore)
     
@@ -142,13 +142,15 @@ def test_ballpark_brute_force():
 def test_strf_fit():
     
     # stimulus features
-    lo_freq = 100 # Hz
+    lo_freq = 1 # Hz
     hi_freq = 1000 # Hz
-    fs = hi_freq*2 # Hz
+    fs = hi_freq * 2 # Hz
     duration = 100 # seconds
     tr_length = 1.0 # seconds
+    tr_sampling_rate = 3 # number of time-samples per TR to plot the STRF in
     time_window = 0.5 # seconds
     freq_window = 256 # this is 2x the number of freq bins we'll end up with in the spectrogram
+    scale_factor = 0.25 # how much to downsample the spectrotemporal space
     
     num_timepoints = np.floor(duration / tr_length)
     
@@ -159,22 +161,29 @@ def test_strf_fit():
     signal = chirp(time, lo_freq, duration, hi_freq, method='linear')
     
     # instantiate an instance of the Stimulus class
-    stimulus = AuditoryStimulus(signal, tr_length, freq_window, time_window, sampling_rate=fs)
+    stimulus = AuditoryStimulus(signal, tr_length, freq_window, time_window, sampling_rate=fs, 
+                                tr_sampling_rate=tr_sampling_rate, scale_factor=scale_factor)
     
     # set up bounds for the grid search
-    bounds = ((lo_freq, hi_freq),(lo_freq, hi_freq),(0,tr_length),(-5,5))
+    bounds = ((lo_freq, hi_freq),(lo_freq, hi_freq),(-5,5))
     
     # initialize the gaussian model
     strf_model = strf.SpectrotemporalModel(stimulus)
     
     # makeup a STRF estimate
-    freq_center = 550 # center frequency
+    freq_center = 500 # center frequency
     freq_sigma = 100 # frequency dispersion
-    time_sigma = 1 # # seconds
+    time_center = 0.5 * stimulus.all_freqs[-1] # this won't change as we'll always convolve over each time-points time-seriesszsz
+    time_sigma = 0.5 * stimulus.all_freqs[-1] # seconds
     hrf_delay = 0.0 # seconds
+    degrees = 0
+
+    g = MakeFastGaussian2D(stimulus.time_coord, stimulus.freq_coord, 
+                           stimulus.all_freqs[-1]*0.5, 500, 
+                           stimulus.all_freqs[-1]*0.5, 100, degrees)
     
     # generate the modeled BOLD response
-    response = strf.compute_model_ts(freq_center, freq_sigma, time_sigma, hrf_delay,
+    response = strf.compute_model_ts(freq_center, freq_sigma, hrf_delay,
                                      stimulus.time_coord, stimulus.freq_coord, stimulus.spectrogram,
                                      tr_length, num_timepoints, time_window, norm_func=utils.zscore)
     
