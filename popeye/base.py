8e62d749aec35d8f6129577725b15c7fcbc2938c
kdesimone <kevindesimone@gmail.com>
2014-09-15 19:20:42 -0400

    DOCS: modified documentation

--- popeye/base.py
+++ popeye/base.py
@@ -7,14 +7,14 @@ Base-classes for poulation encoding models and fits.
 
 
 class PopulationModel(object):
-    """ Abstract class which holds the stimulus model
+    """ Abstract class which holds the PopulationModel
     """
     
     def __init__(self, stimulus):
         self.stimulus = stimulus
 
 class PopulationFit(object):
-    """ Abstract class which holds the fit result of PopulationModel
+    """ Abstract class which holds the PopulationFit
     """
     
     def __init__(self, model, data):
@@ -22,7 +22,7 @@ class PopulationFit(object):
         self.model = model
 
 class StimulusModel(object):
-    """ Abstract class which holds the stimulus model
+    """ Abstract class which holds the StimulusModel
     """
 
     def __init__(self, stim_arr):
