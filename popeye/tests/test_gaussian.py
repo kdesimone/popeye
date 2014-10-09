ea13821b5bab6bb4cdd2aa260629111e5427f7d6
kdesimone <kevindesimone@gmail.com>
2014-10-09 14:45:35 -0400

    TST: fixed the bounds on a test of fit

--- popeye/tests/test_gaussian.py
+++ popeye/tests/test_gaussian.py
@@ -5,7 +5,7 @@ from itertools import repeat
 import numpy as np
 import numpy.testing as npt
 import nose.tools as nt
-import scipy.signal as ss
+from scipy.signal import fftconvolve
 
 import popeye.utilities as utils
 import popeye.gaussian as gaussian
@@ -30,29 +30,30 @@ def test_gaussian_fit():
     bar = simulate_bar_stimulus(pixels_across, pixels_down, viewing_distance, screen_width, thetas, num_steps, ecc)
     
     # create an instance of the Stimulus class
-    stimulus = VisualStimulus(bar, viewing_distance, screen_width, scale_factor, frames_per_tr)
+    stimulus = VisualStimulus(bar, viewing_distance, screen_width, scale_factor)
     
     # set up bounds for the grid search
     search_bounds = ((-10,10),(-10,10),(0.25,5.25),(-5,5),(0.1,1e2))
-    fit_bounds = ((-12,12),(-12,12),(1/(stimulus.ppd*stimulus.scale_factor),12),(-5,5),(0.1,1e2))
+    fit_bounds = ((-12,12),(-12,12),(1/stimulus.ppd,12),(-5,5),(0.1,1e2))
     
     # initialize the gaussian model
     model = gaussian.GaussianModel(stimulus)
     
     # generate a random pRF estimate
     estimate = [-5.24, 2.583, 1.24, -0.25, 2.5]
+    # estimate = [0,0,1,0,1]
     
     # make the stim time-series
     stim = MakeFastGaussPrediction(stimulus.deg_x, stimulus.deg_y, stimulus.stim_arr, estimate[0], estimate[1], estimate[2])
     
     # create the HRF
-    hrf = utils.double_gamma_hrf(estimate[3], tr_length, frames_per_tr)
+    hrf = utils.double_gamma_hrf(estimate[3], tr_length)
     
     # simulate the BOLD response
-    data = ss.fftconvolve(stim,hrf)[0:len(stim)] * estimate[-1]
+    data = fftconvolve(stim,hrf)[0:len(stim)] * estimate[-1]
     
     # fit the response
-    fit = gaussian.GaussianFit(data, model, search_bounds, fit_bounds, tr_length)
+    fit = gaussian.GaussianFit(model, data, search_bounds, fit_bounds, tr_length)
     
     # assert equivalence
     nt.assert_almost_equal(fit.x,estimate[0])
@@ -81,17 +82,17 @@ def test_parallel_gaussian_fit():
     bar = simulate_bar_stimulus(pixels_across, pixels_down, viewing_distance, screen_width, thetas, num_steps, ecc)
     
     # create an instance of the Stimulus class
-    stimulus = VisualStimulus(bar, viewing_distance, screen_width, scale_factor, frames_per_tr)
+    stimulus = VisualStimulus(bar, viewing_distance, screen_width, scale_factor)
     
     # set up bounds for the grid search
     search_bounds = [((-10,10),(-10,10),(0.25,5.25),(-5,5),(0.1,1e2))]*num_voxels
-    fit_bounds = [((-12,12),(-12,12),(1/(stimulus.ppd*stimulus.scale_factor),12),(-5,5),(0.1,1e2))]*num_voxels
+    fit_bounds = [((-12,12),(-12,12),(1/stimulus.ppd,12),(-5,5),(0.1,1e2))]*num_voxels
     
     # make fake voxel indices
     indices = [(1,2,3)]*num_voxels
     
     # initialize the gaussian model
-    gaussian_model = gaussian.GaussianModel(stimulus)
+    model = gaussian.GaussianModel(stimulus)
     
     # generate a random pRF estimate
     estimate = [-5.24, 2.583, 1.24, -0.25, 2.5]
@@ -105,17 +106,17 @@ def test_parallel_gaussian_fit():
         stim = MakeFastGaussPrediction(stimulus.deg_x, stimulus.deg_y, stimulus.stim_arr, estimate[0], estimate[1], estimate[2])
         
         # create the HRF
-        hrf = utils.double_gamma_hrf(estimate[3], tr_length, frames_per_tr)
+        hrf = utils.double_gamma_hrf(estimate[3], tr_length)
         
         # simulate the BOLD response
-        response = ss.fftconvolve(stim,hrf)[0:len(stim)] * estimate[-1]
+        response = fftconvolve(stim,hrf)[0:len(stim)] * estimate[-1]
         
         # append it
         timeseries.append(response)
         
     # package the data structure
-    dat = zip(timeseries,
-              repeat(gaussian_model,num_voxels),
+    dat = zip(repeat(model,num_voxels),
+              timeseries,
               search_bounds,
               fit_bounds,
               repeat(tr_length,num_voxels),
@@ -130,9 +131,9 @@ def test_parallel_gaussian_fit():
     pool.join()
     
     # assert equivalence
-    for gaussian_fit,estimate in zip(output,estimates):
-        nt.assert_almost_equal(gaussian_fit.x,estimate[0])
-        nt.assert_almost_equal(gaussian_fit.y,estimate[1])
-        nt.assert_almost_equal(gaussian_fit.sigma,estimate[2])
-        nt.assert_almost_equal(gaussian_fit.hrf_delay,estimate[3])
+    for fit,estimate in zip(output,estimates):
+        nt.assert_almost_equal(fit.x,estimate[0])
+        nt.assert_almost_equal(fit.y,estimate[1])
+        nt.assert_almost_equal(fit.sigma,estimate[2])
+        nt.assert_almost_equal(fit.hrf_delay,estimate[3])
 
--- popeye/tests/test_xvalidation.py
+++ popeye/tests/test_xvalidation.py
@@ -38,49 +38,48 @@ def test_kfold_xval_repeated_runs():
     num_steps = 20
     ecc = 10
     tr_length = 1.0
-    frames_per_tr = 1.0
     scale_factor = 0.05
     num_runs = 4
     folds = 2
-
+    
     # create the sweeping bar stimulus in memory
     bar = simulate_bar_stimulus(pixels_across, pixels_down, viewing_distance, screen_width, thetas, num_steps, ecc)
-
+    
     # create an instance of the Stimulus class
-    stimulus = VisualStimulus(bar, viewing_distance, screen_width, scale_factor, frames_per_tr)
-
+    stimulus = VisualStimulus(bar, viewing_distance, screen_width, scale_factor)
+    
     # set up bounds for the grid search
     search_bounds = ((-10,10),(-10,10),(0.25,5.25),(-5,5),(0.1,1e2))
-    fit_bounds = ((-12,12),(-12,12),(1/(stimulus.ppd*stimulus.scale_factor),12),(-5,5),(0.1,1e2))
-
+    fit_bounds = ((-12,12),(-12,12),(1/stimulus.ppd,12),(-5,5),(0.1,1e2))
+    
     # initialize the gaussian model
     model = gaussian.GaussianModel(stimulus)
-
+    
     # generate a pRF estimate
     estimate = [-5.24, 2.583, 1.24, -0.25, 2.5]
-
+    
     # create the args context for calling the Fit class
     fit_args = [search_bounds, fit_bounds, tr_length, [0,0,0]]
     fit_kwargs = {'auto_fit': False, 'verbose' : False}    
-
+    
     # create a series of "runs"
     data = np.zeros((stimulus.stim_arr.shape[-1],num_runs))
-
+    
     for r in range(num_runs):
-
+        
         # make the stim time-series
         stim = MakeFastGaussPrediction(stimulus.deg_x, stimulus.deg_y, stimulus.stim_arr, estimate[0], estimate[1], estimate[2])
-
+        
         # create the HRF
-        hrf = utils.double_gamma_hrf(estimate[3], tr_length, frames_per_tr)
-
+        hrf = utils.double_gamma_hrf(estimate[3], tr_length)
+        
         # simulate the BOLD response
         dat = ss.fftconvolve(stim,hrf)[0:len(stim)] * estimate[-1]
-
+        
         # fill out the data list
         data[:,r] = dat
-
-
+        
+        
     # get predictions out for each of the folds ...
     models = (model,)
     left_out_data, predictions = xval.kfold_xval(models, data, gaussian.GaussianFit, folds, fit_args, fit_kwargs)
@@ -110,11 +109,11 @@ def test_kfold_xval_unique_runs():
     bar = simulate_bar_stimulus(pixels_across, pixels_down, viewing_distance, screen_width, thetas, num_steps, ecc)
     
     # create an instance of the Stimulus class
-    stimulus = VisualStimulus(bar, viewing_distance, screen_width, scale_factor, frames_per_tr)
+    stimulus = VisualStimulus(bar, viewing_distance, screen_width, scale_factor)
     
     # set up bounds for the grid search
     search_bounds = ((-10,10),(-10,10),(0.25,5.25),(-5,5),(0.1,1e2))
-    fit_bounds = ((-12,12),(-12,12),(1/(stimulus.ppd*stimulus.scale_factor),12),(-5,5),(0.1,1e2))
+    fit_bounds = ((-12,12),(-12,12),(1/stimulus.ppd,12),(-5,5),(0.1,1e2))
     
     # initialize the gaussian model
     model = gaussian.GaussianModel(stimulus)
