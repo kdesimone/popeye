RF: fixed the polar angle calculation

popeye/og.py
@@ -80,13 +80,13 @@ def recast_estimation_results(output, grid_parent):
                                       fit.rss,
                                       fit.fit_stats[2])
                                  
-            polar[fit.voxel_index] = (np.mod(np.arctan2(fit.x,fit.y),2*np.pi),
-                                     np.sqrt(fit.x**2+fit.y**2),
-                                     fit.sigma,
-                                     fit.beta,
-                                     fit.hrf_delay,
-                                     fit.rss,
-                                     fit.fit_stats[2])
+            polar[fit.voxel_index] = (np.mod(np.arctan2(fit.y,fit.x),2*np.pi)
+                                      np.sqrt(fit.x**2+fit.y**2),
+                                      fit.sigma,
+                                      fit.beta,
+                                      fit.hrf_delay,
+                                      fit.rss,
+                                      fit.fit_stats[2])
                                  
     # get header information from the gridParent and update for the prf volume
     aff = grid_parent.get_affine()