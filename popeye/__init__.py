9925c8772cde9e66a368c08e93de3016131e67b8
kdesimone <kevindesimone@gmail.com>
2014-11-11 13:11:09 -0500

    RF: removed reference to og_zunits

--- popeye/__init__.py
+++ popeye/__init__.py
@@ -8,7 +8,6 @@ from .version import  __version__
 from . import visual_stimulus
 from . import utilities
 from . import og
-from . import og_zunits
 from . import dog
 from . import xvalidation
 from . import plotting
