6d80c8db210159e5e34790da583178a3b5635273
kdesimone <kevindesimone@gmail.com>
2014-12-08 11:45:39 -0500

    RF: small import changes

--- popeye/__init__.py
+++ popeye/__init__.py
@@ -8,8 +8,8 @@ from .version import  __version__
 from . import visual_stimulus
 from . import utilities
 from . import og
+from . import css
 from . import dog
 from . import ogb
-from . import dogb
 from . import xvalidation
 from . import plotting
