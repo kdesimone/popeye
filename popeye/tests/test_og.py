8ab834d54a0b7139785a64c8eec5a039525b83d4
kdesimone <kevindesimone@gmail.com>
2015-02-25 16:49:05 -0500

    TST: added blanks to simulated bar for test

--- popeye/tests/test_og.py
+++ popeye/tests/test_og.py
@@ -29,6 +29,16 @@ def test_og_fit():
     resample_factor = 0.25
     dtype = ctypes.c_short
     
+    # insert blanks
+    thetas = list(thetas)
+    thetas.insert(0,-1)
+    thetas.insert(2,-1)
+    thetas.insert(5,-1)
+    thetas.insert(8,-1)
+    thetas.insert(11,-1)
+    thetas.append(-1)
+    thetas = np.array(thetas)
+    
     # create the sweeping bar stimulus in memory
     bar = simulate_bar_stimulus(pixels_across, pixels_down, viewing_distance, 
                                 screen_width, thetas, num_bar_steps, num_blank_steps, ecc)
