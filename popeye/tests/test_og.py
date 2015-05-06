78125fb05520adf0c8f38e91a7583cac804ca78e
kdesimone <kevindesimone@gmail.com>
2015-05-06 18:34:31 -0400

    TST: fixed test and sped it up

--- popeye/tests/test_og.py
+++ popeye/tests/test_og.py
@@ -28,10 +28,10 @@ def test_og_fit():
     pixels_across = 800 * resample_factor
     pixels_down = 600 * resample_factor
     dtype = ctypes.c_int16
-    Ns = 5
+    Ns = 3
     voxel_index = (1,2,3)
     auto_fit = True
-    verbose = 1
+    verbose = 2
     
     # insert blanks
     thetas = list(thetas)
