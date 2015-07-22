059402e7aa9859b71f0aa95c52c68985c6fb165d
kdesimone <kevindesimone@gmail.com>
2015-07-22 13:00:03 -0400

    RF: testing to see if the hack worked or if it has to do with where tests are run from

--- setup.py
+++ setup.py
@@ -88,10 +88,10 @@ if __name__ == '__main__':
     # spinach.so in the ./popeye/ dir.  this is necessary
     # for importing the Cython methods.  don't know why it
     # doesn't build file in the propery location.
-    import os
-    import shutil
-    srcfile = find('spinach.so','.')
-    dstfile = './popeye/spinach.so'
-    shutil.copy(srcfile, dstfile)
+    # import os
+    # import shutil
+    # srcfile = find('spinach.so','.')
+    # dstfile = './popeye/spinach.so'
+    # shutil.copy(srcfile, dstfile)
     
     
