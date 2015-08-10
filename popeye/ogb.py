d9b14653d49d0eb155be895d36756c6f43ea57de
kdesimone <kevindesimone@gmail.com>
2015-08-10 17:03:21 -0400

    DOC: updating documentation

--- popeye/ogb.py
+++ popeye/ogb.py
@@ -20,14 +20,14 @@ from popeye.spinach import generate_og_receptive_field, generate_rf_timeseries
 class GaussianModel(PopulationModel):
     
     """
-    A Gaussian population receptive field model class
+    A 2D Gaussian population receptive field model class.
     
     """
     
     def __init__(self, stimulus, hrf_model):
         
         """
-        A Gaussian population receptive field model [1]_.
+        A 2D Gaussian population receptive field model [1]_.
         
         Paramaters
         ----------
@@ -35,6 +35,10 @@ class GaussianModel(PopulationModel):
         stimulus : `VisualStimulus` class object
             A class instantiation of the `VisualStimulus` class
             containing a representation of the visual stimulus.
+            
+        hrf_model : callable
+            A function that generates an HRF model given an HRF delay.
+            For more information, see `popeye.utilties.double_gamma_hrf_hrf`
         
         
         References
@@ -117,41 +121,52 @@ class GaussianModel(PopulationModel):
 class GaussianFit(PopulationFit):
     
     """
-    A Gaussian population receptive field fit class
+    A class containing tools for fitting the 2D Gaussian pRF model.
     
     """
     
     def __init__(self, model, data, grids, bounds, Ns,
                  voxel_index=(1,2,3), auto_fit=True, verbose=0):
         
-        
         """
-        A Gaussian population receptive field model [1]_.
-
+        A class containing tools for fitting the 2D Gaussian pRF model.
+        
+        The `GaussianFit` class houses all the fitting tool that are associated with 
+        estimatinga pRF model.  The `GaussianFit` takes a `GaussianModel` instance 
+        `model` and a time-series `data`.  In addition, extent and sampling-rate of a 
+        brute-force grid-search is set with `grids` and `Ns`.  Use `bounds` to set 
+        limits on the search space for each parameter.  
+        
         Paramaters
         ----------
         
+                
+        model : `GaussianModel` class instance
+            An object representing the 2D Gaussian model. 
+        
         data : ndarray
-            An array containing the measured BOLD signal.
+            An array containing the measured BOLD signal of a single voxel.
         
-        model : `GaussianModel` class instance containing the representation
-            of the visual stimulus.
-        
-        search_bounds : tuple
+        grids : tuple
             A tuple indicating the search space for the brute-force grid-search.
             The tuple contains pairs of upper and lower bounds for exploring a
-            given dimension.  For example `fit_bounds=((-10,10),(0,5),)` will
+            given dimension.  For example `grids=((-10,10),(0,5),)` will
             search the first dimension from -10 to 10 and the second from 0 to 5.
-            These values cannot be None. 
+            These values cannot be `None`. 
             
             For more information, see `scipy.optimize.brute`.
         
-        fit_bounds : tuple
+        bounds : tuple
             A tuple containing the upper and lower bounds for each parameter
             in `parameters`.  If a parameter is not bounded, simply use
             `None`.  For example, `fit_bounds=((0,None),(-10,10),)` would 
             bound the first parameter to be any positive number while the
             second parameter would be bounded between -10 and 10.
+        
+        Ns : int
+            Number of samples per stimulus dimension to sample during the ballpark search.
+            
+            For more information, see `scipy.optimize.brute`.
         
         voxel_index : tuple
             A tuple containing the index of the voxel being modeled. The 
@@ -160,20 +175,15 @@ class GaussianFit(PopulationFit):
             indices. With voxel indices, the brain volume can be reconstructed 
             using the newly computed model estimates.
         
-        auto-fit : bool
+        auto_fit : bool
             A flag for automatically running the fitting procedures once the 
             `GaussianFit` object is instantiated.
         
-        verbose : bool
-            A flag for printing some summary information about the model estiamte
-            after the fitting procedures have completed.
+        verbose : int
+            0 = silent
+            1 = print the final solution of an error-minimization
+            2 = print each error-minimization step
         
-        References
-        ----------
-        
-        .. [1] Dumoulin SO, Wandell BA. (2008) Population receptive field 
-        estimates in human visual cortex. NeuroImage 39:647-660
-
         """
         
         PopulationFit.__init__(self, model, data, grids, bounds, Ns, 
@@ -244,19 +254,4 @@ class GaussianFit(PopulationFit):
         return generate_og_receptive_field(self.x, self.y, self.sigma, self.beta, self.baseline,
                                            self.model.stimulus.deg_x,
                                            self.model.stimulus.deg_y) 
-    @auto_attr
-    def msg(self):
-        txt = ("VOXEL=(%.03d,%.03d,%.03d)   TIME=%.03d   RSQUARED=%.02f   STDERR=%.02f   THETA=%.02f   RHO=%.02d   SIGMA=%.02f   BETA=%.08f   BASELINE=%.03f" 
-                %(self.voxel_index[0],
-                  self.voxel_index[1],
-                  self.voxel_index[2],
-                  self.finish-self.start,
-                  self.rsquared,
-                  self.stderr,
-                  self.theta,
-                  self.rho,
-                  self.sigma,
-                  self.beta,
-                  self.baseline))
-        
-        return txt
+                                           
