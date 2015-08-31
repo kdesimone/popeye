fc06f0a61fd1e8426c5544f9f2e1b1a6d4636867
kdesimone <kevindesimone@gmail.com>
2015-08-31 17:25:56 -0400

    DOC: trying to get rtd to show API reference

--- docs/conf.py
+++ docs/conf.py
@@ -172,7 +172,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Popeyedoc'
+htmlhelp_basename = 'popeyedoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
