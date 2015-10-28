f3898793eace2bab0f4d0cb9902e9deb3c43762b
kdesimone <kevindesimone@gmail.com>
2015-10-27 21:35:22 -0400

    RF: embedded the BOLD upsampling + stimulus downsampling within the auditory fit

--- popeye/auditory.py
+++ popeye/auditory.py
@@ -206,7 +206,16 @@ class AuditoryFit(PopulationFit):
         
         # invoke the base class
         PopulationFit.__init__(self, model, data, grids, bounds, Ns, voxel_index, auto_fit, verbose)
-        
+    
+    @auto_attr
+    def data(self, data):
+        # upsample to some rate close to the stimulus
+        source_times = np.linspace(0, 1, self.data, endpoint=True)
+        f = interp1d(source_times, self.data, kind='linear')
+        target_times = np.linspace(0, 1, self.shape[-1]*self.model.stimulus.resample_factor, endpoint=True)
+        resampled_timeseries = f(target_times)
+        return resampled_timeseries
+    
     @auto_attr
     def center_freq0(self):
         return self.ballpark[0]
