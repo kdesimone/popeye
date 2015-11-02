46d5a3ff0b7ffe561f23c74e4404d9944eff07bd
kdesimone <kevindesimone@gmail.com>
2015-11-02 11:50:37 -0500

    DOC: trying to fix autobuild

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.doctest',
               'sphinx.ext.intersphinx',
               'sphinx.ext.pngmath',
+              'sphinx.ext.autosummary',
               ]
 
 # Add any paths that contain templates here, relative to this directory.
