8c05750187bcd68f4b9f8aebaf52928ebc832262
kdesimone <kevindesimone@gmail.com>
2015-12-15 11:50:46 -0500

    TST: fixed test

--- popeye/tests/test_css.py
+++ popeye/tests/test_css.py
@@ -17,10 +17,12 @@ def test_css_fit():
     # stimulus features
     viewing_distance = 38
     screen_width = 25
-    thetas = np.arange(0,360,90)
+    thetas = np.arange(0,360,45)
+    thetas = np.concatenate((np.array([-1]),thetas))
+    thetas = np.concatenate((thetas,np.array([-1])))
     num_blank_steps = 30
     num_bar_steps = 30
-    ecc = 12
+    ecc = 10
     tr_length = 1.0
     frames_per_tr = 1.0
     scale_factor = 0.20
@@ -42,33 +44,36 @@ def test_css_fit():
     # generate a random pRF estimate
     x = -5.24
     y = 2.58
-    sigma = 1.24
+    sigma = 1.23
+    n = 0.90
+    beta = 0.52
+    baseline = 0.1
     hrf_delay = -0.25
-    beta = 1
-    n = 0.33
     
     # create the "data"
-    data =  model.generate_prediction(x, y, sigma, n, beta, hrf_delay)
+    data =  model.generate_prediction(x, y, sigma, n, beta, baseline, hrf_delay)
     
     # set search grid
-    x_grid = (-10,10)
-    y_grid = (-10,10)
-    s_grid = (1/stimulus.ppd,5.25)
-    n_grid = (0.1,0.5)
-    b_grid = (0.01,5)
+    x_grid = (-5,5)
+    y_grid = (-5,5)
+    s_grid = (1/stimulus.ppd,2.75)
+    n_grid = (0.1,0.90)
+    b_grid = (0.01,1)
+    bl_grid = (-2,2)
     h_grid = (-4.0,4.0)
     
     # set search bounds
-    x_bound = (-12.0,12.0)
-    y_bound = (-12.0,12.0)
-    s_bound = (1/stimulus.ppd,12.0)
+    x_bound = (-10,10)
+    y_bound =  (-10,10)
+    s_bound = (1/stimulus.ppd,10)
     n_bound = (1e-8,1.0)
     b_bound = (1e-8,1e5)
+    bl_bound = (None,None)
     h_bound = (-5.0,5.0)
     
     # loop over each voxel and set up a GaussianFit object
-    grids = (x_grid, y_grid, s_grid, n_grid, b_grid, h_grid)
-    bounds = (x_bound, y_bound, s_bound, n_bound, b_bound, h_bound)
+    grids = (x_grid, y_grid, s_grid, n_grid, b_grid, bl_grid, h_grid)
+    bounds = (x_bound, y_bound, s_bound, n_bound, b_bound, bl_bound, h_bound)
     
     # set some meta data
     Ns = 3
@@ -80,9 +85,10 @@ def test_css_fit():
     fit = css.CompressiveSpatialSummationFit(model, data, grids, bounds, Ns, voxel_index, auto_fit, verbose)
     
     # assert equivalence
-    nt.assert_almost_equal(fit.x, x)
-    nt.assert_almost_equal(fit.y, y)
-    nt.assert_almost_equal(fit.sigma, sigma)
-    nt.assert_almost_equal(fit.hrf_delay, hrf_delay)
-    nt.assert_almost_equal(fit.beta, beta)
-    # nt.assert_almost_equal(fit.n, n)
+    nt.assert_almost_equal(fit.x, x, 2)
+    nt.assert_almost_equal(fit.y, y, 2)
+    nt.assert_almost_equal(fit.sigma, sigma, 1)
+    nt.assert_almost_equal(fit.n, n, 1)
+    nt.assert_almost_equal(fit.beta, beta, 1)
+    nt.assert_almost_equal(fit.baseline, baseline, 2)
+    nt.assert_almost_equal(fit.hrf_delay, hrf_delay, 2)
