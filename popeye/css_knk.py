dcedcf886f63b3d29ff7edc1f086de429cc52b17
kdesimone <kevindesimone@gmail.com>
2016-08-24 15:26:21 -0400

    RF: knks css model

---
+++ popeye/css_knk.py
@@ -0,0 +1,229 @@
+#!/usr/bin/python
+
+""" Classes and functions for estimating Compressive Spatial Summation pRF model """
+
+from __future__ import division
+import warnings
+warnings.simplefilter("ignore")
+
+import numpy as np
+from scipy.signal import fftconvolve
+import nibabel
+
+from popeye.onetime import auto_attr
+import popeye.utilities as utils
+from popeye.base import PopulationModel, PopulationFit
+from popeye.spinach import generate_og_receptive_field, generate_rf_timeseries
+
+class CompressiveSpatialSummationModel(PopulationModel):
+    
+    r"""
+    A Compressive Spatial Summation population receptive field model class
+    
+    """
+    
+    def __init__(self, stimulus, hrf_model):
+        
+        r"""
+        A Compressive Spatial Summation population receptive field model [1]_.
+        
+        Paramaters
+        ----------
+        
+        stimulus : `VisualStimulus` class object
+            A class instantiation of the `VisualStimulus` class
+            containing a representation of the visual stimulus.
+        
+        hrf_model : callable
+            A function that generates an HRF model given an HRF delay.
+            For more information, see `popeye.utilties.double_gamma_hrf_hrf`
+        
+        References
+        ----------
+        
+        .. [1] Kay KN, Winawer J, Mezer A, Wandell BA (2014) Compressive spatial
+        summation in human visual cortex. Journal of Neurophysiology 110:481-494.
+        
+        """
+        
+        PopulationModel.__init__(self, stimulus, hrf_model)
+        
+    
+    # main method for deriving model time-series
+    def generate_ballpark_prediction(self, x, y, sigma, n, beta):
+        
+        
+        # modelfun = @(pp,dd) pp(4)*((dd*vflatten(makegaussian2d(res,pp(1),pp(2),pp(3),pp(3),xx,yy,0,0)/(2*pi*pp(3)^2))).^pp(5));
+        
+        # generate the RF
+        rf  = generate_og_receptive_field(x, y, sigma,self.stimulus.deg_x_coarse, self.stimulus.deg_y_coarse)
+        
+        # normalize by integral
+        rf /= (2 * np.pi * sigma**2)
+        
+        # raise to n
+        rf **= n
+        
+        # extract the stimulus time-series
+        mask = np.ones_like(rf, dtype='uint8')
+        response = generate_rf_timeseries(self.stimulus.stim_arr_coarse, rf, mask)
+        
+        # convolve with the HRF
+        hrf = self.hrf_model(self.hrf_delay, self.stimulus.tr_length)
+        
+        # convolve it with the stimulus
+        model = fftconvolve(response, hrf)[0:len(response)]
+        
+        model *= beta
+        
+        return model
+        
+    # main method for deriving model time-series
+    def generate_prediction(self, x, y, sigma, n, beta):
+        
+        # generate the RF
+        rf = generate_og_receptive_field(x, y, sigma, self.stimulus.deg_x, self.stimulus.deg_y)
+        
+        # normalize by integral
+        rf /= (2 * np.pi * sigma**2)
+        
+        # raise to n
+        rf **= n
+        
+        # extract the stimulus time-series
+        mask = np.ones_like(rf, dtype='uint8')
+        response = generate_rf_timeseries(self.stimulus.stim_arr, rf, mask)
+        
+        # convolve with the HRF
+        hrf = self.hrf_model(self.hrf_delay, self.stimulus.tr_length)
+        
+        # convolve it with the stimulus
+        model = fftconvolve(response, hrf)[0:len(response)]
+        
+        model *= beta
+        
+        return model
+        
+class CompressiveSpatialSummationFit(PopulationFit):
+    
+    """
+    A Compressive Spatial Summation population receptive field fit class
+    
+    """
+    
+    def __init__(self, model, data, grids, bounds, Ns,
+                 voxel_index=(1,2,3), auto_fit=True, verbose=0):
+        
+        r"""
+        A class containing tools for fitting the CSS pRF model.
+        
+        The `CompressiveSpatialSummationFit` class houses all the fitting tool that 
+        are associated with estimating a pRF model. The `GaussianFit` takes a 
+        `CompressiveSpatialSummationModel` instance  `model` and a time-series `data`. 
+        In addition, extent and sampling-rate of a  brute-force grid-search is set 
+        with `grids` and `Ns`.  Use `bounds` to set limits on the search space for 
+        each parameter.  
+        
+        Paramaters
+        ----------
+        
+                
+        model : `CompressiveSpatialSummationModel` class instance
+            An object representing the CSS model. 
+        
+        data : ndarray
+            An array containing the measured BOLD signal of a single voxel.
+        
+        grids : tuple
+            A tuple indicating the search space for the brute-force grid-search.
+            The tuple contains pairs of upper and lower bounds for exploring a
+            given dimension.  For example `grids=((-10,10),(0,5),)` will
+            search the first dimension from -10 to 10 and the second from 0 to 5.
+            These values cannot be `None`. 
+            
+            For more information, see `scipy.optimize.brute`.
+        
+        bounds : tuple
+            A tuple containing the upper and lower bounds for each parameter
+            in `parameters`.  If a parameter is not bounded, simply use
+            `None`.  For example, `fit_bounds=((0,None),(-10,10),)` would 
+            bound the first parameter to be any positive number while the
+            second parameter would be bounded between -10 and 10.
+        
+        Ns : int
+            Number of samples per stimulus dimension to sample during the ballpark search.
+            
+            For more information, see `scipy.optimize.brute`.
+        
+        voxel_index : tuple
+            A tuple containing the index of the voxel being modeled. The 
+            fitting procedure does not require a voxel index, but 
+            collating the results across many voxels will does require voxel
+            indices. With voxel indices, the brain volume can be reconstructed 
+            using the newly computed model estimates.
+        
+        auto_fit : bool
+            A flag for automatically running the fitting procedures once the 
+            `GaussianFit` object is instantiated.
+        
+        verbose : int
+            0 = silent
+            1 = print the final solution of an error-minimization
+            2 = print each error-minimization step
+        
+        """
+        
+        PopulationFit.__init__(self, model, data, grids, bounds, Ns, 
+                               voxel_index, auto_fit, verbose)
+    
+    @auto_attr
+    def overloaded_estimate(self):
+        return [self.theta,self.rho,self.sigma,self.n,self.beta,]
+                             
+    @auto_attr
+    def x0(self):
+        return self.ballpark[0]
+        
+    @auto_attr
+    def y0(self):
+        return self.ballpark[1]
+        
+    @auto_attr
+    def s0(self):
+        return self.ballpark[2]
+        
+    @auto_attr
+    def n0(self):
+        return self.ballpark[3]
+        
+    @auto_attr
+    def beta0(self):
+        return self.ballpark[4]
+        
+    @auto_attr
+    def x(self):
+        return self.estimate[0]
+        
+    @auto_attr
+    def y(self):
+        return self.estimate[1]
+        
+    @auto_attr
+    def sigma(self):
+        return self.estimate[2]
+        
+    @auto_attr
+    def n(self):
+        return self.estimate[3]
+        
+    @auto_attr
+    def beta(self):
+        return self.estimate[4]
+        
+    @auto_attr
+    def rho(self):
+        return np.sqrt(self.x**2+self.y**2)
+    
+    @auto_attr
+    def theta(self):
+        return np.mod(np.arctan2(self.y,self.x),2*np.pi)
