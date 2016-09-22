RF: something wrong with the OLS, using simpler rsquared calculation

popeye/base.py
@@ -4,7 +4,7 @@ Base-classes for poulation encoding models and fits.
 
 
 """
-import time
+import time, datetime
 import ctypes
 import numpy as np
 from popeye.onetime import auto_attr
@@ -147,38 +147,20 @@ class PopulationFit(object):
         # automatic fitting
         if self.auto_fit:
             
-            try:
-                # start
-                self.start = time.time()
-                
-                # init
-                self.ballpark
-                
-                # final
-                self.estimate
-                self.overloaded_estimate
-                
-                # performance
-                self.OLS
-                self.rss
-                self.rsquared
-                
-                # finish
-                self.finish = time.time()
-                
-            except:
-                self.finish = time.time()
-                self.rsquared = np.nan
-                self.rss = np.nan
-                self.rsquared = np.nan
-                self.ballpark = np.nan
-                self.estimate = np.nan
-                self.msg = ('VOXEL=(%.03d,%.03d,%.03d)   FAILED TO CONVERGE!'  %(self.voxel_index[0],
-                                                                                 self.voxel_index[1],
-                                                                                 self.voxel_index[2]))
+            # start
+            self.start = datetime.datetime.now()
+            
+            # fit it
+            self.brute_force
+            self.ballpark
+            self.gradient_descent
+            self.estimate
+            
+            # finish
+            self.finish = datetime.datetime.now()
             
             # print
-            if self.verbose and self.rsquared > 0.0:
+            if self.verbose:
                 print(self.msg)
                 
     # the brute search
@@ -192,7 +174,12 @@ class PopulationFit(object):
                                         self.model.generate_ballpark_prediction,
                                         self.very_verbose)
      
-     
+    
+    @auto_attr
+    def duration(self):
+        duration = self.finish-self.start
+        return duration.total_seconds()
+        
     @auto_attr
     def ballpark(self):
         return self.brute_force[0]
@@ -255,9 +242,9 @@ class PopulationFit(object):
     def prediction(self):
         return self.model.generate_prediction(*self.estimate)
     
-    @auto_attr
-    def OLS(self):
-        return utils.ols(self.data,self.prediction)
+    # @auto_attr
+    # def OLS(self):
+    #     return utils.ols(self.data,self.prediction)
     
     @auto_attr
     def coefficient(self):
@@ -265,11 +252,11 @@ class PopulationFit(object):
     
     @auto_attr
     def rsquared(self):
-        return self.OLS.R2adj
+        return np.corrcoef(self.prediction,self.data)[0,1]**2
     
-    @auto_attr
-    def stderr(self):
-        return self.OLS.se[1]
+    # @auto_attr
+    # def stderr(self):
+    #     return self.OLS.se[1]
     
     @auto_attr
     def rss(self):
@@ -277,12 +264,13 @@ class PopulationFit(object):
     
     @auto_attr
     def msg(self):
+        txt = '%s' % (np.round(self.overloaded_estimate,4))
         if self.auto_fit is True and self.overloaded_estimate is not None:
             txt = ("VOXEL=(%.03d,%.03d,%.03d)   TIME=%.03d   RSQ=%.02f  EST=%s"
                 %(self.voxel_index[0],
                   self.voxel_index[1],
                   self.voxel_index[2],
-                  self.finish-self.start,
+                  np.round(self.duration),
                   self.rsquared,
                   np.round(self.overloaded_estimate,4)))
         elif self.auto_fit is True:
@@ -290,7 +278,7 @@ class PopulationFit(object):
                 %(self.voxel_index[0],
                   self.voxel_index[1],
                   self.voxel_index[2],
-                  self.finish-self.start,
+                  np.round(self.duration),
                   self.rsquared,
                   np.round(self.estimate,4)))
         else: