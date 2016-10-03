a30de850f70059666885eb95c7e2b11f895bc725
kdesimone <kevindesimone@gmail.com>
2016-10-03 15:48:49 -0400

    ENH: added spm_hrf model

--- popeye/utilities.py
+++ popeye/utilities.py
@@ -22,6 +22,47 @@ from numpy.random import randn, seed
 import sharedmem
 import cPickle
 
+def spm_hrf(delay, TR):
+    """ An implementation of spm_hrf.m from the SPM distribution
+
+Arguments:
+
+Required:
+TR: repetition time at which to generate the HRF (in seconds)
+
+Optional:
+p: list with parameters of the two gamma functions:
+                                                     defaults
+                                                    (seconds)
+   p[0] - delay of response (relative to onset)         6
+   p[1] - delay of undershoot (relative to onset)      16
+   p[2] - dispersion of response                        1
+   p[3] - dispersion of undershoot                      1
+   p[4] - ratio of response to undershoot               6
+   p[5] - onset (seconds)                               0
+   p[6] - length of kernel (seconds)                   32
+
+"""
+    # default settings
+    p=[6,16,1,1,6,0,32]
+    p=[float(x) for x in p]
+    
+    # delay variation
+    p[0] += delay
+    p[1] += delay
+    
+    fMRI_T = 16.0
+    
+    TR=float(TR)
+    dt  = TR/fMRI_T
+    u   = np.arange(p[6]/dt + 1) - p[5]/dt
+    hrf=stats.gamma.pdf(u,p[0]/p[2],scale=1.0/(dt/p[2])) - stats.gamma.pdf(u,p[1]/p[3],scale=1.0/(dt/p[3]))/p[4]
+    good_pts=np.array(range(np.int(p[6]/TR)))*fMRI_T
+    hrf=hrf[list(good_pts)]
+    # hrf = hrf([0:(p(7)/RT)]*fMRI_T + 1);
+    hrf = hrf/np.sum(hrf);
+    return hrf
+
 def recast_estimation_results(output, grid_parent, overloaded=False):
     
     # load the gridParent
