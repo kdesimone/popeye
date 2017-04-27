b2a1de37d86a0f340515eac709206f3d3eb5bd33
Kevin DeSimone <kevindesimone@gmail.com>
2017-04-27 00:05:34 -0400

    TST: added test for steerable strf

---
+++ popeye/tests/test_spatiotemporal_2dcos.py
@@ -0,0 +1,106 @@
+import os
+from itertools import repeat
+import ctypes
+
+import numpy as np
+import numpy.testing as npt
+import nose.tools as nt
+
+import popeye.utilities as utils
+from popeye import spatiotemporal_2dcos as strf
+from popeye.visual_stimulus import VisualStimulus, simulate_bar_stimulus, resample_stimulus
+
+def test_strf_2dcos_fit():
+    
+    viewing_distance = 38
+    screen_width = 25
+    thetas = np.tile(np.arange(0,360,90),2)
+    thetas = np.insert(thetas,0,-1)
+    thetas = np.append(thetas,-1)
+    num_blank_steps = 20
+    num_bar_steps = 20
+    ecc = 10
+    tr_length = 1.0
+    frames_per_tr = 1.0
+    scale_factor = 1.0
+    pixels_down = 200
+    pixels_across = 200
+    dtype = ctypes.c_int16
+    auto_fit = True
+    verbose = 1
+    projector_hz = 480
+    tau = 0.0875
+    mask_size = 5
+    hrf = 0.25
+    power = 0
+    
+    # create the sweeping bar stimulus in memory
+    stim = simulate_bar_stimulus(pixels_across, pixels_down, viewing_distance, 
+                                 screen_width, thetas, num_bar_steps, num_blank_steps, ecc)
+    
+    # create an instance of the Stimulus class
+    stimulus = VisualStimulus(stim, viewing_distance, screen_width, scale_factor, tr_length, dtype)
+    stimulus.fps = projector_hz
+    flicker_vec = np.zeros_like(stim[0,0,:]).astype('uint8')
+    flicker_vec[1*20:5*20] = 1
+    flicker_vec[5*20:9*20] = 2
+    stimulus.flicker_vec = flicker_vec
+    stimulus.flicker_hz = [10,20]
+    
+    # initialize the gaussian model
+    model = strf.SpatioTemporalModel(stimulus, utils.double_gamma_hrf)
+    model.tau = tau
+    model.hrf_delay = hrf
+    model.mask_size = mask_size
+    model.power = 0
+    
+    # generate a random pRF estimate
+    x = -2.24
+    y = 1.58
+    sigma = 1.23
+    weight = 0.90
+    beta = 1.0
+    baseline = -0.25
+    
+    # create the "data"
+    data =  model.generate_prediction(x, y, sigma, weight, beta, baseline)
+    
+    # set search grid
+    x_grid = utils.grid_slice(-8.0,7.0,5)
+    y_grid = utils.grid_slice(-8.0,7.0,5)
+    s_grid = utils.grid_slice(0.75,3.0,5)
+    w_grid = utils.grid_slice(0.05,0.95,5)
+    
+    # set search bounds
+    x_bound = (-10,10)
+    y_bound =  (-10,10)
+    s_bound = (1/stimulus.ppd,10)
+    w_bound = (1e-8,1.0)
+    b_bound = (1e-8,1e5)
+    u_bound = (None, None)
+    
+    # loop over each voxel and set up a GaussianFit object
+    grids = (x_grid, y_grid, s_grid, w_grid,)
+    bounds = (x_bound, y_bound, s_bound, w_bound, b_bound, u_bound)
+    
+    # fit the response
+    fit = strf.SpatioTemporalFit(model, data, grids, bounds)
+    
+    # coarse fit
+    npt.assert_almost_equal((fit.x0,fit.y0,fit.s0,fit.w0,fit.beta0,fit.baseline0),(-0.5, 3.25, 3.0, 0.49999999999999994, 1.0000000000000002, -0.010826240688006666),2)
+    
+    # fine fit
+    npt.assert_almost_equal(fit.x, x, 1)
+    npt.assert_almost_equal(fit.y, y, 1)
+    npt.assert_almost_equal(fit.sigma, sigma, 1)
+    npt.assert_almost_equal(fit.weight, weight, 1)
+    npt.assert_almost_equal(fit.beta, beta, 1)
+    npt.assert_almost_equal(fit.baseline, baseline, 1)
+    
+    # overloaded
+    npt.assert_almost_equal(fit.overloaded_estimate, [2.5270727137292481,
+                                                      2.7416305841622624,
+                                                      1.2256761293512328,
+                                                      0.89789336800034436,
+                                                      0.99962425175020264,
+                                                      -0.25009416568850351],1)
