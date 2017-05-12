53a9c6dca6af25e8c2426a0f774b33939c904623
Kevin DeSimone <kevindesimone@gmail.com>
2017-05-12 11:19:49 -0400

    DOC: extending documentation to the og model

--- popeye/og.py
+++ popeye/og.py
@@ -48,7 +48,7 @@ class GaussianModel(PopulationModel):
     def generate_ballpark_prediction(self, x, y, sigma):
         
         r"""
-        Predict signal for the Gaussian Model using the downsampled stimulus´.
+        Predict signal for the Gaussian Model using the downsampled stimulus.
         The rate of stimulus downsampling is defined in `model.stimulus.scale_factor`.
         
         Parameters
@@ -149,18 +149,19 @@ class GaussianModel(PopulationModel):
         __________
         x : float
             Horizontal location of the Gaussian RF.
-        
+            
         y: float 
             Vertical location of the Gaussian RF.
-        
+            
         sigma: float
             Dipsersion of the Gaussian RF.
-        
+            
         """
         
-        return generate_og_receptive_field(x, y, sigma,
-                                           self.stimulus.deg_x,
-                                           self.stimulus.deg_y)
+        rf = generate_og_receptive_field(x, y, sigma, self.stimulus.deg_x, self.stimulus.deg_y)
+        rf /= (2 * np.pi * sigma**2) * 1/np.diff(self.stimulus.deg_x[0,0:2])**2
+        
+        return rf
         
 class GaussianFit(PopulationFit):
     
@@ -184,56 +185,59 @@ class GaussianFit(PopulationFit):
         Paramaters
         ----------
         
-                
+        
         model : `GaussianModel` class instance
             An object representing the 2D Gaussian model. 
-        
+            
         data : ndarray
             An array containing the measured BOLD signal of a single voxel.
-        
-        grids : tuple
+            
+        grids : tuple or Slice Object
             A tuple indicating the search space for the brute-force grid-search.
             The tuple contains pairs of upper and lower bounds for exploring a
             given dimension.  For example `grids=((-10,10),(0,5),)` will
             search the first dimension from -10 to 10 and the second from 0 to 5.
-            These values cannot be `None`. 
-            
+            The resolution of this search space is set with the `Ns` argument. 
             For more information, see `scipy.optimize.brute`.
-        
+            
+            Alternatively you can pass `grids` a Slice Object. If you do use this
+            option, you do not need to specificy `Ns` in `GaussianFit`. See 
+            `popeye.utilities.grid_slice` for more details.
+            
         bounds : tuple
             A tuple containing the upper and lower bounds for each parameter
             in `parameters`.  If a parameter is not bounded, simply use
             `None`.  For example, `fit_bounds=((0,None),(-10,10),)` would 
             bound the first parameter to be any positive number while the
             second parameter would be bounded between -10 and 10.
-        
-        
+            
         voxel_index : tuple
             A tuple containing the index of the voxel being modeled. The 
             fitting procedure does not require a voxel index, but 
             collating the results across many voxels will does require voxel
             indices. With voxel indices, the brain volume can be reconstructed 
             using the newly computed model estimates.
-        
+            
         Ns : int
             Number of samples per stimulus dimension to sample during the ballpark search.
-            
             For more information, see `scipy.optimize.brute`.
-        
+            
+            This can be `None` if `grids` is a tuple of Slice Objects.
+            
         auto_fit : bool
             A flag for automatically running the fitting procedures once the 
             `GaussianFit` object is instantiated.
-        
+            
         verbose : int
             0 = silent
             1 = print the final solution of an error-minimization
             2 = print each error-minimization step
-        
+            
         """
         
         PopulationFit.__init__(self, model, data, grids, bounds, 
-                               voxel_index, Ns, auto_fit, verbose)
-    
+                           voxel_index, Ns, auto_fit, verbose)
+                           
     @auto_attr
     def overloaded_estimate(self):
         return [self.theta, self.rho, self.sigma, self.beta, self.baseline]
@@ -302,7 +306,5 @@ class GaussianFit(PopulationFit):
         
         r""" Returns the fitted Gaussian pRF. """
         
-        return generate_og_receptive_field(self.x, self.y, self.sigma,
-                                           self.model.stimulus.deg_x,
-                                           self.model.stimulus.deg_y)
+        return self.model.generate_receptive_field(self.x, self.y, self.sigma)
                                            
