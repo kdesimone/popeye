RF: fixed the fine tuned base/baseline calculation to pick up right where gridfit left off

popeye/auditory.py
@@ -47,7 +47,7 @@ class AuditoryModel(PopulationModel):
         # invoke the base class
         PopulationModel.__init__(self, stimulus, hrf_model)
     
-    def generate_ballpark_prediction(self, center_freq, sigma, unscaled=False):
+    def generate_ballpark_prediction(self, center_freq, sigma):
 
         r"""
         Generate a prediction for the 1D Gaussian model.
@@ -87,21 +87,18 @@ class AuditoryModel(PopulationModel):
         # units
         model = (model - np.mean(model)) / np.mean(model)
         
-        if unscaled:
-            return model
-        else:
-            # regress out mean and linear
-            p = linregress(model, self.data)
-            
-            # offset
-            model += p[1]
-            
-            # scale
-            model *= np.abs(p[0])
-            
-            return model
+        # regress out mean and linear
+        p = linregress(model, self.data)
         
-    def generate_prediction(self, center_freq, sigma, beta, baseline):
+        # offset
+        model += p[1]
+        
+        # scale
+        model *= np.abs(p[0])
+        
+        return model
+        
+    def generate_prediction(self, center_freq, sigma, beta, baseline, unscaled=False):
         
         r"""
         Generate a prediction for the 1D Gaussian auditory pRF model.
@@ -144,13 +141,16 @@ class AuditoryModel(PopulationModel):
         # units
         model = (model - np.mean(model)) / np.mean(model)
         
-        # offset
-        model += baseline
-        
-        # scale it
-        model *= beta
-        
-        return model
+        if unscaled:
+            return model
+        else:
+            # offset
+            model += baseline
+            
+            # scale it
+            model *= beta
+            
+            return model
         
 class AuditoryFit(PopulationFit):
     

popeye/css.py
@@ -50,7 +50,7 @@ class CompressiveSpatialSummationModel(PopulationModel):
         PopulationModel.__init__(self, stimulus, hrf_model, cached_model_path, nuisance)
         
     # main method for deriving model time-series
-    def generate_ballpark_prediction(self, x, y, sigma, n, unscaled=False):
+    def generate_ballpark_prediction(self, x, y, sigma, n):
         
         # generate the RF
         rf = generate_og_receptive_field(x, y, sigma,self.stimulus.deg_x0, self.stimulus.deg_y0)
@@ -73,22 +73,19 @@ class CompressiveSpatialSummationModel(PopulationModel):
         # units
         model = (model - np.mean(model)) / np.mean(model)
         
-        if unscaled:
-            return model
-        else:
-            # regress out mean and linear
-            p = linregress(model, self.data)
-            
-            # offset
-            model += p[1]
-            
-            # scale
-            model *= np.abs(p[0])
-            
-            return model
+        # regress out mean and linear
+        p = linregress(model, self.data)
+        
+        # offset
+        model += p[1]
+        
+        # scale
+        model *= np.abs(p[0])
+        
+        return model
         
     # main method for deriving model time-series
-    def generate_prediction(self, x, y, sigma, n, beta, baseline):
+    def generate_prediction(self, x, y, sigma, n, beta, baseline, unscaled=False):
         
         # generate the RF
         rf = generate_og_receptive_field(x, y, sigma, self.stimulus.deg_x, self.stimulus.deg_y)
@@ -111,13 +108,17 @@ class CompressiveSpatialSummationModel(PopulationModel):
         # convert units
         model = (model - np.mean(model)) / np.mean(model)
         
-        # offset
-        model += baseline
-        
-        # scale it by beta
-        model *= beta
-        
-        return model
+        if unscaled:
+            return model
+        else:
+            
+            # offset
+            model += baseline
+            
+            # scale it by beta
+            model *= beta
+            
+            return model
         
 class CompressiveSpatialSummationFit(PopulationFit):
     

popeye/dog.py
@@ -14,11 +14,11 @@ from scipy.signal import fftconvolve
 from scipy.integrate import trapz
 
 import nibabel
-
+import numexpr as ne
 from popeye.onetime import auto_attr
 import popeye.utilities as utils
 from popeye.base import PopulationModel, PopulationFit
-from popeye.spinach import generate_og_receptive_field, generate_rf_timeseries_nomask
+from popeye.spinach import generate_og_receptive_field, generate_rf_timeseries
 
 class DifferenceOfGaussiansModel(PopulationModel):
     
@@ -54,7 +54,7 @@ class DifferenceOfGaussiansModel(PopulationModel):
         PopulationModel.__init__(self, stimulus, hrf_model)
         
         
-    def generate_ballpark_prediction(self, x, y, sigma, sigma_ratio, volume_ratio, unscaled=False):
+    def generate_ballpark_prediction(self, x, y, sigma, sigma_ratio, volume_ratio):
         
         # extract the center response
         rf_center = generate_og_receptive_field(x, y, sigma, self.stimulus.deg_x0, self.stimulus.deg_y0)
@@ -64,10 +64,47 @@ class DifferenceOfGaussiansModel(PopulationModel):
                                                   self.stimulus.deg_x0, self.stimulus.deg_x0) * 1/sigma_ratio**2
         
         # difference
-        rf = rf_center - np.sqrt(volume_ratio)*rf_surround
+        rf = ne.evaluate('rf_center - sqrt(volume_ratio)*rf_surround')
         
         # extract the response
-        response = generate_rf_timeseries_nomask(self.stimulus.stim_arr0, rf)
+        mask = self.distance_mask(x, y, sigma*sigma_ratio)
+        response = generate_rf_timeseries(self.stimulus.stim_arr0, rf, mask)
+        
+        # generate the hrf
+        hrf = self.hrf_model(self.hrf_delay, self.stimulus.tr_length)
+        
+        # convolve it
+        model = fftconvolve(response, hrf)[0:len(response)]
+        
+        # units
+        model = (model-np.mean(model)) / np.mean(model)
+        
+        # regress out mean and linear
+        p = linregress(model, self.data)
+        
+        # offset
+        model += p[1]
+        
+        # scale
+        model *= np.abs(p[0])
+        
+        return model
+        
+    def generate_prediction(self, x, y, sigma, sigma_ratio, volume_ratio, beta, baseline, unscaled=False):
+        
+        # extract the center response
+        rf_center = generate_og_receptive_field(x, y, sigma, self.stimulus.deg_x, self.stimulus.deg_y)
+        
+        # extract surround response
+        rf_surround = generate_og_receptive_field(x, y, sigma*sigma_ratio, 
+                                                  self.stimulus.deg_x, self.stimulus.deg_y) * 1/sigma_ratio**2
+        
+        # difference
+        rf = ne.evaluate('rf_center - sqrt(volume_ratio)*rf_surround')
+        
+        # extract the response
+        mask = self.distance_mask(x, y, sigma*sigma_ratio)
+        response = generate_rf_timeseries(self.stimulus.stim_arr, rf, mask)
         
         # generate the hrf
         hrf = self.hrf_model(self.hrf_delay, self.stimulus.tr_length)
@@ -81,48 +118,14 @@ class DifferenceOfGaussiansModel(PopulationModel):
         if unscaled:
             return model
         else:
-            # regress out mean and linear
-            p = linregress(model, self.data)
             
             # offset
-            model += p[1]
+            model += baseline
             
-            # scale
-            model *= np.abs(p[0])
+            # scale it by beta
+            model *= beta
             
             return model
-        
-    def generate_prediction(self, x, y, sigma, sigma_ratio, volume_ratio, beta, baseline):
-        
-        # extract the center response
-        rf_center = generate_og_receptive_field(x, y, sigma, self.stimulus.deg_x, self.stimulus.deg_y)
-        
-        # extract surround response
-        rf_surround = generate_og_receptive_field(x, y, sigma*sigma_ratio, 
-                                                  self.stimulus.deg_x, self.stimulus.deg_y) * 1/sigma_ratio**2
-        
-        # difference
-        rf = rf_center - np.sqrt(volume_ratio)*rf_surround
-        
-        # extract the response
-        response = generate_rf_timeseries_nomask(self.stimulus.stim_arr, rf)
-        
-        # generate the hrf
-        hrf = self.hrf_model(self.hrf_delay, self.stimulus.tr_length)
-        
-        # convolve it
-        model = fftconvolve(response, hrf)[0:len(response)]
-        
-        # units
-        model = (model-np.mean(model)) / np.mean(model)
-        
-        # offset
-        model += baseline
-        
-        # scale it by beta
-        model *= beta
-        
-        return model
             
     # DoG receptive field
     def receptive_field(self, x, y, sigma, sigma_ratio, volume_ratio):
@@ -206,9 +209,8 @@ class DifferenceOfGaussiansFit(PopulationFit):
     
     @auto_attr
     def overloaded_estimate(self):
-       return [self.theta, self.rho, self.sigma, self.beta, self.baseline]
-
-
+       return [self.theta, self.rho, self.sigma, self.sigma_ratio, self.volume_ratio, self.beta, self.baseline]
+       
     @auto_attr
     def overloaded_ballpark(self):
        return np.append(self.ballpark, (self.beta0, self.baseline0))

popeye/og.py
@@ -18,7 +18,7 @@ from popeye.spinach import generate_og_receptive_field, generate_rf_timeseries
 
 class GaussianModel(PopulationModel):
     
-    def __init__(self, stimulus, hrf_model, nuisance=None):
+    def __init__(self, stimulus, hrf_model, cached_model_path=None, nuisance=None):
         
         r"""A 2D Gaussian population receptive field model [1]_.
         
@@ -42,10 +42,10 @@ class GaussianModel(PopulationModel):
         
         """
         
-        PopulationModel.__init__(self, stimulus, hrf_model, nuisance)
+        PopulationModel.__init__(self, stimulus, hrf_model, cached_model_path, nuisance)
         
     # main method for deriving model time-series
-    def generate_ballpark_prediction(self, x, y, sigma, unscaled=False):
+    def generate_ballpark_prediction(self, x, y, sigma):
         
         r"""
         Predict signal for the Gaussian Model using the downsampled stimulus.
@@ -80,22 +80,19 @@ class GaussianModel(PopulationModel):
         # units
         model = (model-np.mean(model)) / np.mean(model)
         
-        if unscaled:
-            return model
-        else:
-            # regress out mean and linear
-            p = linregress(model, self.data)
-            
-            # offset
-            model += p[1]
-            
-            # scale
-            model *= np.abs(p[0])
-            
-            return model
+        # regress out mean and linear
+        p = linregress(model, self.data)
+        
+        # offset
+        model += p[1]
+        
+        # scale
+        model *= np.abs(p[0])
+        
+        return model
             
     # main method for deriving model time-series
-    def generate_prediction(self, x, y, sigma, beta, baseline):
+    def generate_prediction(self, x, y, sigma, beta, baseline, unscaled=False):
         
         r"""
         Predict signal for the Gaussian Model.
@@ -135,13 +132,17 @@ class GaussianModel(PopulationModel):
         # units
         model = (model-np.mean(model)) / np.mean(model)
         
-        # offset
-        model += baseline
-        
-        # scale it by beta
-        model *= beta
-        
-        return model
+        if unscaled:
+            return model
+        else:
+            
+            # offset
+            model += baseline
+            
+            # scale it by beta
+            model *= beta
+            
+            return model
     
     def generate_receptive_field(self, x, y, sigma):
         
@@ -244,11 +245,14 @@ class GaussianFit(PopulationFit):
     @auto_attr
     def overloaded_estimate(self):
         return [self.theta, self.rho, self.sigma, self.beta, self.baseline]
-    
+
     
     @auto_attr
     def overloaded_ballpark(self):
-        return np.append(self.ballpark, (self.beta0, self.baseline0))
+        overload = np.append(self.ballpark, (self.beta0, self.baseline0))
+        if self.very_verbose:
+            print('The final solution of the gridfit is %s!' %(overload))
+        return overload  
     
     @auto_attr
     def x0(self):

popeye/og_hrf.py
@@ -18,7 +18,7 @@ from popeye.spinach import generate_og_receptive_field, generate_rf_timeseries
 
 class GaussianModel(PopulationModel):
     
-    def __init__(self, stimulus, hrf_model, nuisance=None):
+    def __init__(self, stimulus, hrf_model, cached_model_path=None, nuisance=None):
         
         r"""A 2D Gaussian population receptive field model [1]_,[2]_.
         
@@ -45,10 +45,10 @@ class GaussianModel(PopulationModel):
         
         """
         
-        PopulationModel.__init__(self, stimulus, hrf_model, nuisance)
+        PopulationModel.__init__(self, stimulus, hrf_model, cached_model_path, nuisance)
     
     # main method for deriving model time-series
-    def generate_ballpark_prediction(self, x, y, sigma, hrf_delay, unscaled=False):
+    def generate_ballpark_prediction(self, x, y, sigma, hrf_delay):
         
         r"""
         Predict signal for the Gaussian Model using the downsampled stimulus.
@@ -89,22 +89,19 @@ class GaussianModel(PopulationModel):
         # units
         model = (model-np.mean(model)) / np.mean(model)
         
-        if unscaled:
-            return model
-        else:
-            # regress out mean and linear
-            p = linregress(model, self.data)
-            
-            # offset
-            model += p[1]
-            
-            # scale
-            model *= np.abs(p[0])
-            
-            return model
+        # regress out mean and linear
+        p = linregress(model, self.data)
+        
+        # offset
+        model += p[1]
+        
+        # scale
+        model *= np.abs(p[0])
+        
+        return model
         
     # main method for deriving model time-series
-    def generate_prediction(self, x, y, sigma, hrf_delay, beta, baseline):
+    def generate_prediction(self, x, y, sigma, hrf_delay, beta, baseline, unscaled=False):
         
         r"""
         Predict signal for the Gaussian Model.
@@ -151,14 +148,17 @@ class GaussianModel(PopulationModel):
         # units
         model = (model-np.mean(model)) / np.mean(model)
         
-        # offset
-        model += baseline
-        
-        # scale it by beta
-        model *= beta
-        
-        return model
+        if unscaled:
+            return model
+        else:
+            # offset
+            model += baseline
     
+            # scale it by beta
+            model *= beta
+    
+            return model
+
     def generate_receptive_field(self, x, y, sigma):
         
         r"""

popeye/spatiotemporal.py
@@ -45,7 +45,7 @@ class SpatioTemporalModel(PopulationModel):
         PopulationModel.__init__(self, stimulus, hrf_model)
     
     
-    def generate_ballpark_prediction(self, x, y, sigma, weight, unscaled=False):
+    def generate_ballpark_prediction(self, x, y, sigma, weight):
         
         r"""
         Predict signal for the Gaussian Model using the downsampled stimulus.
@@ -91,21 +91,18 @@ class SpatioTemporalModel(PopulationModel):
         # units
         model = (model - np.mean(model)) / np.mean(model)
         
-        if unscaled:
-            return model
-        else:
-            # regress out mean and linear
-            p = linregress(model, self.data)
-            
-            # offset
-            model += p[1]
-            
-            # scale
-            model *= np.abs(p[0])
-            
-            return model
+        # regress out mean and linear
+        p = linregress(model, self.data)
+        
+        # offset
+        model += p[1]
+        
+        # scale
+        model *= np.abs(p[0])
+        
+        return model
     
-    def generate_prediction(self, x, y, sigma, weight, beta, baseline):
+    def generate_prediction(self, x, y, sigma, weight, beta, baseline, unscaled=False):
         
         r"""
         Predict signal for the Gaussian Model using the full resolution stimulus.
@@ -157,13 +154,17 @@ class SpatioTemporalModel(PopulationModel):
         # units
         model = (model - np.mean(model)) / np.mean(model)
         
-        # offset
-        model += baseline
-        
-        # scale it by beta
-        model *= beta
-        
-        return model
+        if unscaled:
+            return model
+        else:
+            
+            # offset
+            model += baseline
+            
+            # scale it by beta
+            model *= beta
+            
+            return model
     
     @auto_attr
     def p(self):

popeye/spatiotemporal_css.py
@@ -52,7 +52,7 @@ class SpatioTemporalModel(PopulationModel):
         
         
     # for the final solution, we use spatiotemporal
-    def generate_ballpark_prediction(self, x, y, sigma, n, weight, unscaled=False):
+    def generate_ballpark_prediction(self, x, y, sigma, n, weight):
         
         # mask for speed
         mask = self.distance_mask_coarse(x, y, sigma)
@@ -79,22 +79,19 @@ class SpatioTemporalModel(PopulationModel):
         # units
         model = (model - np.mean(model)) / np.mean(model)
         
-        if unscaled:
-            return model
-        else:
-            # regress out mean and linear
-            p = linregress(model, self.data)
-            
-            # offset
-            model += p[1]
-            
-            # scale
-            model *= np.abs(p[0])
-            
-            return model
+        # regress out mean and linear
+        p = linregress(model, self.data)
+        
+        # offset
+        model += p[1]
+        
+        # scale
+        model *= np.abs(p[0])
+        
+        return model
         
     # for the final solution, we use spatiotemporal
-    def generate_prediction(self, x, y, sigma, n, weight, beta, baseline):
+    def generate_prediction(self, x, y, sigma, n, weight, beta, baseline, unscaled=False):
         
         # mask for speed
         mask = self.distance_mask(x, y, sigma)
@@ -121,13 +118,16 @@ class SpatioTemporalModel(PopulationModel):
         # convert units
         model = (model - np.mean(model)) / np.mean(model)
         
-        # scale
-        model *= beta
-        
-        # offset
-        model += baseline
-        
-        return model
+        if unscaled:
+            return model
+        else:
+            # scale
+            model *= beta
+            
+            # offset
+            model += baseline
+            
+            return model
 
     @auto_attr
     def p(self):

popeye/spatiotemporal_hrf.py
@@ -46,7 +46,7 @@ class SpatioTemporalModel(PopulationModel):
     
     
     # for the final solution, we use spatiotemporal
-    def generate_ballpark_prediction(self, x, y, sigma, weight, hrf_delay, unscaled=False):
+    def generate_ballpark_prediction(self, x, y, sigma, weight, hrf_delay):
         
         r"""
         Predict signal for the Gaussian Model using the downsampled stimulus.
@@ -98,22 +98,19 @@ class SpatioTemporalModel(PopulationModel):
         # units
         model = (model - np.mean(model)) / np.mean(model)
         
-        if unscaled:
-            return model
-        else:
-            # regress out mean and linear
-            p = linregress(model, self.data)
-            
-            # offset
-            model += p[1]
-            
-            # scale
-            model *= np.abs(p[0])
-            
-            return model
+        # regress out mean and linear
+        p = linregress(model, self.data)
+        
+        # offset
+        model += p[1]
+        
+        # scale
+        model *= np.abs(p[0])
+        
+        return model
     
     # for the final solution, we use spatiotemporal
-    def generate_prediction(self, x, y, sigma, weight, hrf_delay, beta, baseline):
+    def generate_prediction(self, x, y, sigma, weight, hrf_delay, beta, baseline, unscaled=False):
         
         r"""
         Predict signal for the Gaussian Model using the full resolution stimulus.
@@ -171,13 +168,17 @@ class SpatioTemporalModel(PopulationModel):
         # convert units
         model = (model - np.mean(model)) / np.mean(model)
         
-        # scale
-        model *= beta
-        
-        # offset
-        model += baseline
-        
-        return model
+        if unscaled:
+            return model
+        else:
+            
+            # scale
+            model *= beta
+            
+            # offset
+            model += baseline
+            
+            return model
     
     @auto_attr
     def p(self):