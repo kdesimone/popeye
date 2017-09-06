0f6cf09fb96eb157ad06f0972c1a6c3ae2557153
Kevin DeSimone <kevindesimone@gmail.com>
2017-09-06 17:19:40 -0400

    RF: removing new cython build

--- setup.py
+++ setup.py
@@ -52,20 +52,31 @@ opts = dict(name=NAME,
             install_requires=install_requires
             )
 
-try:
-    from distutils.extension import Extension
-    from Cython.Distutils import build_ext as build_pyx_ext
-    from numpy import get_include
-    # add Cython extensions to the setup options
-    exts = [Extension('popeye.spinach',
-                      ['popeye/spinach.pyx'],
-                       include_dirs=[get_include()]),
-                      ]
-    opts['cmdclass'] = dict(build_ext=build_pyx_ext)
-    opts['ext_modules'] = exts
-except ImportError:
-    # no loop for you!
-    pass
+# try:
+#     from distutils.extension import Extension
+#     from Cython.Distutils import build_ext as build_pyx_ext
+#     from numpy import get_include
+#     # add Cython extensions to the setup options
+#     exts = [Extension('popeye.spinach',
+#                       ['popeye/spinach.pyx'],
+#                        include_dirs=[get_include()],
+#                        extra_compile_args=['-O3', '-march=native', '-mtune=native', '-funroll-loops', '-xHOST', '-fast'])
+#                       ]
+#     opts['cmdclass'] = dict(build_ext=build_pyx_ext)
+#     opts['ext_modules'] = exts
+# except:
+#     
+# normal build
+from distutils.extension import Extension
+from Cython.Distutils import build_ext as build_pyx_ext
+from numpy import get_include
+# add Cython extensions to the setup options
+exts = [Extension('popeye.spinach',
+                  ['popeye/spinach.pyx'],
+                   include_dirs=[get_include()]),
+                  ]
+opts['cmdclass'] = dict(build_ext=build_pyx_ext)
+opts['ext_modules'] = exts
 
 # For some commands, use setuptools.  Note that we do NOT list install here!
 # If you want a setuptools-enhanced install, just run 'setupegg.py install'
