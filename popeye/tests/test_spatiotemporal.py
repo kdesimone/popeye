8a6b1427f0034b88ed8d2cac9aff96269368d410
kdesimone <kevindesimone@gmail.com>
2017-09-20 23:00:19 -0400

    TST: matching new grid_slice

--- popeye/tests/test_spatiotemporal.py
+++ popeye/tests/test_spatiotemporal.py
@@ -89,7 +89,14 @@ def test_strf_fit():
     fit = strf.SpatioTemporalFit(model, data, grids, bounds)
     
     # coarse fit
-    npt.assert_almost_equal((fit.x0,fit.y0,fit.sigma0,fit.weight0,fit.beta0,fit.baseline0),[-0.5, 3.25, 2.4375, 0.95, 1.0409, -0.25],4)
+    ballpark = [-2.0,
+                 1.0,
+                 1.2,
+                 0.94999999999999996,
+                 0.97634618265361939,
+                 -0.24999999999999986]
+                 
+    npt.assert_almost_equal((fit.x0,fit.y0,fit.sigma0,fit.weight0,fit.beta0,fit.baseline0),ballpark,4)
     
     # fine fit
     npt.assert_almost_equal(fit.x, x)
