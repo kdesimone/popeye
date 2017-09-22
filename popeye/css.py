fcfbca44862727c3c0639faa04215377502caa5a
kdesimone <kevindesimone@gmail.com>
2017-09-22 08:37:24 -0400

    RF: removed abs() of the slope

--- popeye/css.py
+++ popeye/css.py
@@ -80,7 +80,7 @@ class CompressiveSpatialSummationModel(PopulationModel):
         model += p[1]
         
         # scale
-        model *= np.abs(p[0])
+        model *= p[0]
         
         return model
         
