acf1290d79af77330b95420867756ab864a7caf5
kdesimone <kevindesimone@gmail.com>
2017-09-28 00:20:54 -0400

    RF: added new tests for neg/pos amplitude control

--- popeye/tests/test_og.py
+++ popeye/tests/test_og.py
@@ -161,6 +161,11 @@ def test_negative_og_fit():
     npt.assert_almost_equal(fit.sigma, sigma, 2)
     npt.assert_almost_equal(fit.beta, beta, 2)
     
+    nt.assert_false(fit.model.bounded_amplitude)
+    nt.assert_true(fit.slope<0)
+    nt.assert_true(fit.beta0<0)
+    nt.assert_true(fit.beta<0)
+    
     # test receptive field
     rf = generate_og_receptive_field(x, y, sigma, fit.model.stimulus.deg_x, fit.model.stimulus.deg_y)
     rf /= (2 * np.pi * sigma**2) * 1/np.diff(model.stimulus.deg_x[0,0:2])**2
@@ -169,6 +174,71 @@ def test_negative_og_fit():
     # test model == fit RF
     npt.assert_almost_equal(np.round(fit.model.generate_receptive_field(x,y,sigma).sum()), np.round(fit.receptive_field.sum()))
 
+def test_bounded_amplitude_failure():
+
+    # stimulus features
+    viewing_distance = 38
+    screen_width = 25
+    thetas = np.arange(0,360,90)
+    thetas = np.insert(thetas,0,-1)
+    thetas = np.append(thetas,-1)
+    num_blank_steps = 30
+    num_bar_steps = 30
+    ecc = 12
+    tr_length = 1.0
+    frames_per_tr = 1.0
+    scale_factor = 1.0
+    pixels_across = 100
+    pixels_down = 100
+    dtype = ctypes.c_int16
+
+    # create the sweeping bar stimulus in memory
+    bar = simulate_bar_stimulus(pixels_across, pixels_down, viewing_distance, 
+                                screen_width, thetas, num_bar_steps, num_blank_steps, ecc)
+                                
+    # create an instance of the Stimulus class
+    stimulus = VisualStimulus(bar, viewing_distance, screen_width, scale_factor, tr_length, dtype)
+    
+    # initialize the gaussian model
+    model = og.GaussianModel(stimulus, utils.double_gamma_hrf)
+    model.hrf_delay = 0
+    model.mask_size = 6
+    
+    # generate a random pRF estimate
+    x = -5.24
+    y = 2.58
+    sigma = 1.24
+    beta = -0.25
+    baseline = 0.25
+    
+    # create the "data"
+    data = model.generate_prediction(x, y, sigma, beta, baseline)
+    
+    # set search grid
+    x_grid = utils.grid_slice(-10,10,5)
+    y_grid = utils.grid_slice(-10,10,5)
+    s_grid = utils.grid_slice (0.25,5.25,5)
+
+    # set search bounds
+    x_bound = (-12.0,12.0)
+    y_bound = (-12.0,12.0)
+    s_bound = (0.001,12.0)
+    b_bound = (1e-8,None)
+    m_bound = (None,None)
+
+    # loop over each voxel and set up a GaussianFit object
+    grids = (x_grid, y_grid, s_grid,)
+    bounds = (x_bound, y_bound, s_bound, b_bound, m_bound)
+
+    # fit the response
+    fit = og.GaussianFit(model, data, grids, bounds)
+    
+    nt.assert_true(fit.model.bounded_amplitude)
+    nt.assert_true(fit.slope>0)
+    nt.assert_true(fit.beta0>0)
+    nt.assert_true(fit.beta>0)
+    nt.assert_true(fit.beta != beta)
+    
 # def test_og_nuisance_fit():
 #     
 #     # stimulus features
